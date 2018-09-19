5ebbd0d017bcaed98eba923d13702a86062f66af
Richard Lucas <sherlockdoge@gmail.com>
2018-09-19 16:25:04 +0100

    Create new solution, add .gitignore file

---
+++ Advent2017_Day1/Advent2017_Day1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Advent2017_Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
