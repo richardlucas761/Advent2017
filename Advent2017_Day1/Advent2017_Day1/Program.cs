f51dd2a14fc757386f4a158826e36aeb5d093636
Richard Lucas <sherlockdoge@gmail.com>
2018-09-22 12:02:58 +0100

    Include failing unit tests.

--- Advent2017_Day1/Advent2017_Day1/Program.cs
+++ Advent2017_Day1/Advent2017_Day1/Program.cs
@@ -7,8 +7,14 @@ namespace Advent2017_Day1
         private static void Main()
         {
             const string test1 = "1122";
+            const string test2 = "1111";
+            const string test3 = "1234";
+            const string test4 = "91212129";
 
             Console.WriteLine($"{test1} = {Solver.Solve(test1)}");
+            Console.WriteLine($"{test2} = {Solver.Solve(test2)}");
+            Console.WriteLine($"{test3} = {Solver.Solve(test3)}");
+            Console.WriteLine($"{test4} = {Solver.Solve(test4)}");
         }
     }
 }
--- Advent2017_Day1/Advent2017_Day1/Solver.cs
+++ Advent2017_Day1/Advent2017_Day1/Solver.cs
@@ -4,7 +4,33 @@
     {
         public static int Solve(string puzzle)
         {
-            return -1;
+            var sum = 0;
+
+            for (var i = 0; i < puzzle.Length; i++)
+            {
+                if (i + 1 == puzzle.Length)
+                {
+                    if (puzzle[i] == puzzle[0])
+                    {
+                        sum = UpdateSum(puzzle, sum, i);
+                    }
+                }
+                else
+                {
+                    if (puzzle[i] == puzzle[i + 1])
+                    {
+                        sum = UpdateSum(puzzle, sum, i);
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        private static int UpdateSum(string puzzle, int sum, int i)
+        {
+            sum = (int) (sum + char.GetNumericValue(puzzle[i]));
+            return sum;
         }
     }
 }
